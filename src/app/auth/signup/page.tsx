a21799dcda8053442273b9f5e4c9dc037474b98e
ayoooyh <syh9566@naver.com>
2025-03-16 17:05:00 +0900

    feat : height 수정

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Signup() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen py-15">
       <Link href="/">
         <Image
           src="/images/logo.svg"
