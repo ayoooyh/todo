591d68df1d8d90ab0e9081679181c29331a4bf00
ayoooyh <syh9566@naver.com>
2025-03-16 16:12:53 +0900

    feat : signUp 타입 추가

---
+++ src/types/auth/auth.ts
@@ -0,0 +1,21 @@
+export interface ISignUpRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface ISignUpResponse {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  email: string;
+  hashed_password: string;
+  name: string;
+}
+
+export interface IAuthField {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
