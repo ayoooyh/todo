35cd9e90f2332c1af7f1611322d9a94063e308e7
ayoooyh <syh9566@naver.com>
2025-03-16 19:42:22 +0900

    feat : signIn api 추가

--- src/apis/auth/auth.ts
+++ src/apis/auth/auth.ts
@@ -1,7 +1,20 @@
 import axiosInstance from "../axiosInstance";
-import { ISignUpRequest } from "@/types/auth";
+import { ISignUpRequest, ISignInRequest, ISignInResponse } from "@/types/auth";
 
 export const signUp = async (data: ISignUpRequest) => {
   const response = await axiosInstance.post("/user", data);
   return response.data;
+};
+
+export const signIn = async (data: ISignInRequest) => {
+  const response = await axiosInstance.post<ISignInResponse>(
+    "/auth/login",
+    data
+  );
+
+  if (response.data.access_token) {
+    return response.data;
+  } else {
+    throw new Error("토큰없음");
+  }
 };
