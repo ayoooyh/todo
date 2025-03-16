62248db164233638f306663c877eb937b98bf89e
ayoooyh <syh9566@naver.com>
2025-03-16 16:42:33 +0900

    feat : container 불필요성으로 삭제

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,9 +1,20 @@
-import SignUpContainer from "@/containers/auth/SignUpContainer";
+import SignUp from "@/components/auth/SignUp";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Signup() {
   return (
-    <div>
-      <SignUpContainer />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Link href="/">
+        <Image
+          src="/images/logo.svg"
+          alt="logo"
+          width={270}
+          height={89}
+          className="mb-10"
+        />
+      </Link>
+      <SignUp />
     </div>
   );
 }
