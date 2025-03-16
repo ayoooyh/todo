2ddcb8da98915d07b1f56890c2d52d9e73a76c3e
ayoooyh <syh9566@naver.com>
2025-03-16 16:13:22 +0900

    feat : signUp 페이지 수정

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignUpContainer from "@/containers/auth/SignUpContainer";
+
+export default function Signup() {
+  return (
+    <div>
+      <SignUpContainer />
+    </div>
+  );
+}
---
+++ src/containers/auth/SignUpContainer.tsx
@@ -0,0 +1,20 @@
+import SignUp from "@/components/auth/SignUp";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function SignUpContainer() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Link href="/">
+        <Image
+          src="/images/logo.svg"
+          alt="logo"
+          width={270}
+          height={89}
+          className="mb-10"
+        />
+      </Link>
+      <SignUp />
+    </div>
+  );
+}
