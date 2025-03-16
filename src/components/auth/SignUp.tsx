1ab16945df8b9f936d896cc2500f7f224fd8321f
ayoooyh <syh9566@naver.com>
2025-03-16 16:45:21 +0900

    move : AuthInput 파일이동 및 경로 수정

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { ISignUpRequest } from "@/types/auth/auth";
 import { signUp } from "@/apis/auth/auth";
 import { useForm, RegisterOptions } from "react-hook-form";
-import { AuthInput } from "@/components/common/AuthInput";
+import { AuthInput } from "@/components/auth/AuthInput";
 import { IAuthField } from "@/types/auth/auth";
 import { useRouter } from "next/navigation";
 
