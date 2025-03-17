32ca222124d086ba933c52a94eb6a94e580c7cd9
ayoooyh <syh9566@naver.com>
2025-03-17 18:04:45 +0900

    feat : middleware 추가

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const accessToken = request.cookies.get("access_token");
+  const refreshToken = request.cookies.get("refresh_token");
+
+  const protectedRoutes = ["/"];
+  const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname);
+
+  if (!accessToken && !refreshToken && isProtectedRoute) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
+  if (
+    (accessToken || refreshToken) &&
+    request.nextUrl.pathname === "/auth/signin"
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/auth/signin"],
+};
