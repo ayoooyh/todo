951e18592e2d99371b25d181fe33fad2de267d3a
ayoooyh <syh9566@naver.com>
2025-03-17 15:08:28 +0900

    feat : 토스트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +15,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          style: {
+            fontSize: "14px",
+            padding: "14px",
+            maxWidth: "500px",
+            alignItems: "center",
+            justifyContent: "center",
+          },
+        }}
+      />
     </html>
   );
 }
