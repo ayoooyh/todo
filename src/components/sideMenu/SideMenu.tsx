0c79b218cadf4b2802e5caa58ab74f1c590b9da1
ayoooyh <syh9566@naver.com>
2025-03-18 13:22:09 +0900

    move : 파일이동

---
+++ src/components/sideMenu/SideMenu.tsx
@@ -0,0 +1,62 @@
+import User from "@/components/sideMenu/User";
+import Goal from "@/components/sideMenu/Goal";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function SideMenu() {
+  const [isFolded, setIsFolded] = useState(false);
+
+  return (
+    <div
+      className={`flex flex-col bg-white h-screen transition-all duration-300 ${
+        isFolded ? "w-[60px]" : "max-w-[280px]"
+      }`}
+    >
+      <div
+        className={`flex ${
+          isFolded ? "flex-col gap-4" : "justify-between"
+        } items-center px-4 py-4`}
+      >
+        {isFolded ? (
+          <Image
+            src="/images/mini-logo.svg"
+            alt="logo"
+            width={32}
+            height={32}
+          />
+        ) : (
+          <Image src="/images/logo.svg" alt="logo" width={106} height={35} />
+        )}
+        <Image
+          src={isFolded ? "/images/expand.svg" : "/images/fold.svg"}
+          alt={isFolded ? "expand" : "fold"}
+          width={24}
+          height={24}
+          className="cursor-pointer"
+          onClick={() => setIsFolded(!isFolded)}
+        />
+      </div>
+
+      {!isFolded && (
+        <div className="flex flex-col gap-4">
+          <User />
+          <div className="border-t border-slate-200" />
+          <div className="flex justify-left items-center gap-2 px-6">
+            <Image
+              src="/images/home.svg"
+              alt="dashBoard"
+              width={24}
+              height={24}
+            />
+            <Link href="/" className="text-lg font-medium text-slate-800">
+              대시보드
+            </Link>
+          </div>
+          <div className="border-t border-slate-200" />
+          <Goal />
+        </div>
+      )}
+    </div>
+  );
+}
