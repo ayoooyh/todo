271d6535223bbfbc731e61d824056359c5a7c58c
ayoooyh <syh9566@naver.com>
2025-03-22 18:01:32 +0900

    refactor : useGoalId 간소화

--- src/hooks/useGoalId.ts
+++ src/hooks/useGoalId.ts
@@ -1,9 +1,8 @@
 import { useParams } from "next/navigation";
 
 export const useGoalId = () => {
-  const params = useParams();
-  const goalId = typeof params?.id === "string" ? parseInt(params.id) : null;
-  // URL 파라미터에서 id를 추출하여 숫자로 변환
+  const params = useParams<{ id: string }>();
+  const goalId = Number(params.id);
 
   return goalId;
 };
