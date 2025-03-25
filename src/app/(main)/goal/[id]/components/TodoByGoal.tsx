cd954268171d0d196b63d2d9fc4c1bc17dcb6fb1
ayoooyh <syh9566@naver.com>
2025-03-25 21:28:43 +0900

    refactor : suspense, useMemo를 사용하여 리펙토링

--- src/app/(main)/goal/[id]/components/TodoByGoal.tsx
+++ src/app/(main)/goal/[id]/components/TodoByGoal.tsx
@@ -1,24 +1,26 @@
 "use client";
 
-import { useGetTodosQuery } from "@/queries/useTodoQuery";
+import { Suspense, useMemo } from "react";
+import {
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+} from "@/queries/useTodoQuery";
 import Image from "next/image";
 import CreateTodo from "../../../../../components/CreateTodo";
 import { useState } from "react";
-import { useUpdateTodoMutation } from "@/queries/useTodoQuery";
 import { useGoalId } from "@/hooks/useGoalId";
+import { ErrorBoundary } from "react-error-boundary";
+import { ITodo } from "@/types/todo";
 
-export default function TodoByGoal() {
-  const goalId = useGoalId();
+const TodoContainer = ({ goalId }: { goalId: number }) => {
+  const { data: todosData } = useGetTodosQuery({ goalId });
+  const todos = useMemo(() => todosData?.todos || [], [todosData]);
 
-  const {
-    data: todos,
-    isLoading: todosLoading,
-    error: todosError,
-  } = useGetTodosQuery({ goalId });
-
-  const { mutate: updateTodo } = useUpdateTodoMutation();
-
-  const goalTodos = todos?.todos;
+  const undoneTodos = useMemo(
+    () => todos.filter((todo) => !todo.done),
+    [todos]
+  );
+  const doneTodos = useMemo(() => todos.filter((todo) => todo.done), [todos]);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -30,108 +32,127 @@ export default function TodoByGoal() {
     setIsModalOpen(false);
   };
 
-  if (todosLoading) return <div>로딩 중...</div>;
-  if (todosError) return <div>에러가 발생했습니다: {todosError.message}</div>;
-  if (!todos) return <div>데이터가 없습니다.</div>;
+  return (
+    <div className="flex gap-4">
+      <div className="flex flex-col gap-1 w-1/2 bg-white rounded-xl p-4 h-[228px]">
+        <div className="flex items-center justify-between">
+          <span className="text-slate-800 font-semibold text-sm">To do</span>
+          <button className="flex items-center gap-1" onClick={handleModalOpen}>
+            <Image
+              src="/images/plus-blue.svg"
+              alt="plus"
+              width={16}
+              height={16}
+            />
+            <span className="text-sm text-blue-500 font-semibold">
+              할일 추가
+            </span>
+          </button>
+          {isModalOpen && <CreateTodo onClose={handleModalClose} />}
+        </div>
+        <Todos todos={undoneTodos} />
+      </div>
+
+      <div className="flex flex-col gap-1 w-1/2 bg-slate-200 rounded-xl p-4 h-[228px]">
+        <span className="text-slate-800 font-semibold text-sm">Done</span>
+        <DoneTodos todos={doneTodos} />
+      </div>
+    </div>
+  );
+};
+
+const DoneTodos = ({ todos }: { todos: ITodo[] }) => {
+  const { mutate: updateTodo } = useUpdateTodoMutation();
+
+  if (todos.length === 0) {
+    return (
+      <div className="flex items-center justify-center flex-1 min-h-[100px]">
+        <span className="text-sm text-neutral-500">다 한 일이 아직 없어요</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="py-2 px-4 flex flex-col gap-2">
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className="flex items-center gap-2 text-neutral-700 text-sm"
+        >
+          <input
+            type="checkbox"
+            checked={true} // 항상 체크 상태
+            onChange={() =>
+              updateTodo({
+                todoId: todo.id,
+                title: todo.title,
+                done: false, // 체크박스 체크 시 할일 완료 처리
+                fileUrl: todo.file_url,
+                linkUrl: todo.link_url,
+                goalId: todo.goal_id,
+              })
+            }
+            className="cursor-pointer"
+          />
+          {todo.title}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Todos = ({ todos }: { todos: ITodo[] }) => {
+  const { mutate: updateTodo } = useUpdateTodoMutation();
+
+  if (todos.length === 0) {
+    return (
+      <div className="flex items-center justify-center flex-1 min-h-[100px]">
+        <span className="text-sm text-neutral-500">
+          해야할 일이 아직 없어요
+        </span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="py-2 px-4 flex flex-col gap-2">
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className="flex items-center gap-2 text-neutral-700 text-sm"
+        >
+          <input
+            type="checkbox"
+            checked={false} // 항상 체크 해제 상태
+            onChange={() =>
+              updateTodo({
+                todoId: todo.id,
+                title: todo.title,
+                done: true, // 체크박스 체크 시 할일 완료 처리
+                fileUrl: todo.file_url,
+                linkUrl: todo.link_url,
+                goalId: todo.goal_id,
+              })
+            }
+            className="cursor-pointer"
+          />
+          {todo.title}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default function TodoByGoal() {
+  const goalId = useGoalId();
 
   return (
     <>
-      <div className="flex gap-4">
-        <div className="flex flex-col gap-1 w-1/2 bg-white rounded-xl p-4 h-[228px]">
-          <div className="flex items-center justify-between">
-            <span className="text-slate-800 font-semibold text-sm">To do</span>
-            <button
-              className="flex items-center gap-1"
-              onClick={handleModalOpen}
-            >
-              <Image
-                src="/images/plus-blue.svg"
-                alt="plus"
-                width={16}
-                height={16}
-              />
-              <span className="text-sm text-blue-500 font-semibold">
-                할일 추가
-              </span>
-            </button>
-            {isModalOpen && <CreateTodo onClose={handleModalClose} />}
-          </div>
-          {goalTodos?.filter((todo) => !todo.done).length === 0 ? (
-            <div className="flex items-center justify-center flex-1 min-h-[100px]">
-              <span className="text-sm text-neutral-500">
-                해야할 일이 아직 없어요
-              </span>
-            </div>
-          ) : (
-            <div className="py-2 px-4 flex flex-col gap-2">
-              {goalTodos
-                ?.filter((todo) => !todo.done)
-                .map((todo) => (
-                  <div
-                    key={todo.id}
-                    className="flex items-center gap-2 text-neutral-700 text-sm"
-                  >
-                    <input
-                      type="checkbox"
-                      checked={todo.done}
-                      onChange={() =>
-                        updateTodo({
-                          todoId: todo.id,
-                          title: todo.title,
-                          done: !todo.done,
-                          fileUrl: todo.file_url,
-                          linkUrl: todo.link_url,
-                          goalId: goalId as number,
-                        })
-                      }
-                      className="cursor-pointer"
-                    />
-                    {todo.title}
-                  </div>
-                ))}
-            </div>
-          )}
-        </div>
-
-        <div className="flex flex-col gap-1 w-1/2 bg-slate-200 rounded-xl p-4 h-[228px]">
-          <span className="text-slate-800 font-semibold text-sm">Done</span>
-          {goalTodos?.filter((todo) => todo.done).length === 0 ? (
-            <div className="flex items-center justify-center flex-1 min-h-[100px]">
-              <span className="text-sm text-neutral-500">
-                다 한 일이 아직 없어요
-              </span>
-            </div>
-          ) : (
-            <div className="py-2 px-4 flex flex-col gap-2">
-              {goalTodos
-                ?.filter((todo) => todo.done)
-                .map((todo) => (
-                  <div
-                    key={todo.id}
-                    className="flex items-center gap-2 text-neutral-700 text-sm"
-                  >
-                    <input
-                      type="checkbox"
-                      checked={todo.done}
-                      onChange={() =>
-                        updateTodo({
-                          todoId: todo.id,
-                          title: todo.title,
-                          done: !todo.done,
-                          fileUrl: todo.file_url,
-                          linkUrl: todo.link_url,
-                          goalId: goalId as number,
-                        })
-                      }
-                      className="cursor-pointer"
-                    />
-                    {todo.title}
-                  </div>
-                ))}
-            </div>
-          )}
-        </div>
-      </div>
+      <ErrorBoundary fallback={<div>에러가 발생했습니다.</div>}>
+        <Suspense fallback={<div>로딩 중</div>}>
+          <TodoContainer goalId={goalId as number} />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }
