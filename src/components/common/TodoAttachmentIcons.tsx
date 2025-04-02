18fb8b17547fddcec770dc30f395c94adee0d5ed
ayoooyh <syh9566@naver.com>
2025-04-02 15:13:06 +0900

    feat : 할일 첨부 등 아이콘 모음 컴포넌트 추가

---
+++ src/components/common/TodoAttachmentIcons.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import EditAndDelete from "./EditAndDelete";
+import { useState, useRef, useEffect } from "react";
+import { ITodo } from "@/types/todo";
+
+interface TodoAttachmentIconsProps {
+  fileUrl?: string | null;
+  linkUrl?: string | null;
+  todoId: number;
+  todo: ITodo;
+}
+
+const TodoAttachmentIcons = ({
+  fileUrl,
+  linkUrl,
+  todoId,
+  todo,
+}: TodoAttachmentIconsProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleKebabClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div ref={menuRef} className="flex gap-2 relative">
+      {fileUrl && (
+        <Image src="/images/file.svg" alt="file" width={24} height={24} />
+      )}
+      {linkUrl && (
+        <Image src="/images/link.svg" alt="link" width={24} height={24} />
+      )}
+      <button className="cursor-pointer" onClick={handleKebabClick}>
+        <Image src="/images/kebab.svg" alt="kebab" width={24} height={24} />
+      </button>
+      {isOpen && <EditAndDelete todoId={todoId} todo={todo} />}
+    </div>
+  );
+};
+
+export default TodoAttachmentIcons;
