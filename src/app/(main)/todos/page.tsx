c5a746d0eab74a183a4e775473ce8e5792cc9846
ayoooyh <syh9566@naver.com>
2025-04-02 22:53:40 +0900

    style : 로딩 시 css 위치 수정

--- src/app/(main)/todos/page.tsx
+++ src/app/(main)/todos/page.tsx
@@ -62,7 +62,11 @@ export default function TodosPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        Loading...
+      </div>
+    );
   }
 
   const handleModalOpen = () => {
