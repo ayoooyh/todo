6d7774f0d7cd4cab70094e7b37841513a0e9c0e4
ayoooyh <syh9566@naver.com>
2025-04-04 13:42:54 +0900

    comment : 수정예정 부분 주석 추가

--- src/queries/useTodoQuery.tsx
+++ src/queries/useTodoQuery.tsx
@@ -38,6 +38,7 @@ export const useGetTodosQuery = ({
         sortOrder,
       }),
     staleTime: 5 * 60 * 1000,
+    //TODO: 토큰 관련 부분 해결 후 수정예정, 임시로 retry 1로 설정
     retry: 1,
     refetchOnWindowFocus: false,
   });
