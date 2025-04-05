d2401dddbcdf98ed82508ede4a26073e67311d0e
ayoooyh <syh9566@naver.com>
2025-04-05 15:38:11 +0900

    feat : useGetNotesQuery 추가

---
+++ src/queries/useNoteQuery.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { getNotes } from "@/apis/notes";
+import { INotes } from "@/types/note";
+
+export const useGetNotesQuery = ({
+  goal_id,
+  cursor,
+  size = 20,
+}: {
+  goal_id: number;
+  cursor?: string;
+  size?: number;
+}) => {
+  return useQuery<INotes>({
+    queryKey: ["notes", goal_id, size, cursor],
+    queryFn: async () => {
+      const response = await getNotes({ goal_id, size, cursor });
+      return response;
+    },
+    enabled: true,
+    staleTime: 5 * 60 * 1000,
+  });
+};
