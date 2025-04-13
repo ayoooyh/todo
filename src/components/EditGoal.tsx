c01757e379b775075518cca45eb3c839e73ec3fd
ayoooyh <syh9566@naver.com>
2025-04-13 17:09:31 +0900

    feat : 목표 수정 컴포넌트 생성

--- src/components/EditGoal.tsx
+++ src/components/EditGoal.tsx
@@ -1,8 +1,50 @@
 "use client";
 
 import Image from "next/image";
+import { useUpdateGoalMutation } from "@/queries/dashBoard/useGoalQuery";
+import { useForm } from "react-hook-form";
+import { IPostAndUpdateGoals } from "@/types/goal";
+import { useGetGoalQuery } from "@/queries/dashBoard/useGoalQuery";
+import { useRouter } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
 
-export default function EditGoal({ onClose }: { onClose: () => void }) {
+export default function EditGoal({
+  goalId,
+  onClose,
+}: {
+  goalId: number;
+  onClose: () => void;
+}) {
+  const { mutate: updateGoal } = useUpdateGoalMutation();
+  const { data: goalData } = useGetGoalQuery({ goalId });
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<IPostAndUpdateGoals>({
+    mode: "onChange",
+    defaultValues: {
+      title: goalData?.title || "",
+    },
+  });
+
+  const onSubmit = (data: IPostAndUpdateGoals) => {
+    updateGoal(
+      { goalId, updateData: data },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["goal", goalId] });
+          queryClient.invalidateQueries({ queryKey: ["goals"] });
+          onClose();
+          router.refresh();
+        },
+      }
+    );
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-10">
       <div className="bg-white p-6 rounded-lg w-[300px]">
@@ -13,18 +55,28 @@ export default function EditGoal({ onClose }: { onClose: () => void }) {
               <Image src="/images/exit.svg" alt="exit" width={24} height={24} />
             </button>
           </div>
-          <div className="flex flex-col gap-2">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-2"
+          >
             <span className="text-base font-semibold text-slate-800">
               목표 이름
             </span>
             <input
+              {...register("title", { required: "제목을 입력해주세요" })}
               type="text"
               className="w-full bg-slate-50 rounded-xl px-6 py-3 text-base font-normal text-slate-800"
             />
-          </div>
-          <button className="w-full bg-blue-500 text-white rounded-xl px-6 py-3 hover:bg-blue-600">
-            수정하기
-          </button>
+            <button
+              type="submit"
+              disabled={!isValid}
+              className={`w-full bg-blue-500 text-white rounded-xl px-6 py-3 hover:bg-blue-600 ${
+                !isValid ? "bg-slate-400 cursor-not-allowed" : ""
+              }`}
+            >
+              수정하기
+            </button>
+          </form>
         </div>
       </div>
     </div>
